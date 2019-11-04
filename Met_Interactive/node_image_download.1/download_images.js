38b99dda15b669d0ce5fbc5d000af8869f51fec3
EC2 Default User <ec2-user@ip-172-31-57-179.ec2.internal>
2019-11-04 18:16:47 +0000

    interactive updates

---
+++ Met_Interactive/node_image_download.1/download_images.js
@@ -0,0 +1,34 @@
+// load a default library that lets us read/write to the file system
+let fs = require('fs');
+// load a default library that lets us make HTTP requests (like calls to an API)
+let request = require('request');
+
+// the folder we will write into
+let folder = "treePaintings";
+
+// download the image by url, name the file by filename
+function downloadImage(uri, filename, callback){
+  request.head(uri, function(err, res, body) {
+    // console.log('content-type:', res.headers['content-type']);
+    // console.log('content-length:', res.headers['content-length']);
+    request(uri).pipe(fs.createWriteStream( folder + "/" + filename)).on('close', callback);
+  });
+}
+
+// go through the json we created before
+function downloadData() {
+  fs.readFile("treeData5.json", "utf8", (err, data) => {
+    if (err) console.log(err);
+
+    JSON.parse(data).forEach(e => {
+      if (e.filename != '' & e.filename != null) {
+        console.log('Downloading ' + e.filename);
+        downloadImage(e.primaryImage, e.filename, function(){
+          console.log('Finished Downloading ' + e.filename);
+        });
+      }
+    });
+  });
+}
+
+downloadData();
