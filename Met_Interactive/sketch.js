ecf21c72bf0a35700a362381bb5aa788a4aedb43
EC2 Default User <ec2-user@ip-172-31-57-179.ec2.internal>
2019-11-18 02:15:15 +0000

    updates

--- Met_Interactive/sketch.js
+++ Met_Interactive/sketch.js
@@ -1,11 +1,7 @@
 // MS1 Project 3: Interactive w/ Met Dataset
-// Amanda Anderson-You, Nov. 2019
-// P5 display sliver images
-
-
 let data;
 
-// bring in approx 440 sliver images made from 30 paintings using jimp.js
+// bring in approx 500 sliver images made from 30 paintings using jimp.js
 // each sliver image is saved at 80px wide, 800 px tall
 function preload() {
   data = loadJSON('sliversObjects.json');
@@ -13,11 +9,12 @@ function preload() {
 
 // use 'visual' to link into css file
 function setup() {
-  var canvas = createCanvas(1200, 1000);
+  var canvas = createCanvas(windowWidth, windowHeight);
+  // canvas.style('display', 'block');
   // canvas.parent('visual');
+  background('black');
   displayImages(data);
 }
-
 
 // use random to select a random object from the json data, do this 14 times.
 function displayImages(newData) {
@@ -35,15 +32,9 @@ function loadSliver(index, imageObject){
       var x = (index+1)*45
       imgWidth = 60
       imgHeight = imgWidth*10
-      image(sliver, 100+(index+1)*imgWidth, 400-((imageObject.horizon*imgWidth)*10), imgWidth,imgHeight);
-      // image(sliver, (index+1)*imgWidth, 650-((imageObject.horizon*imgWidth)*10),imgWidth,imgHeight);
+      image(sliver, 100+(index+1)*imgWidth, 530-((imageObject.horizon*imgWidth)*10), imgWidth,imgHeight);
     });
 }
 
-// To Do:
-// solve horizon height - jumping around as page reloads, can I keep the horizon at a steady level?
-// keep longer slices from being cropped out
-// on button click run displayImages function to refresh slivers
-// determine final size of img width/height after layed out with css - verify parent above
-// finalize total json file
-// solve for duplicates, consider not having same painting side by side
+
+
